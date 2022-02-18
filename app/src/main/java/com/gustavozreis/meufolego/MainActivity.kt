3d3565fc5553cbf533feb9a592181987efe2a09b
gustavozreis <gzreis@gmail.com>
2022-02-18 20:03:10 -0300

    Configurado o componente de navegação por completo

--- app/src/main/java/com/gustavozreis/meufolego/MainActivity.kt
+++ app/src/main/java/com/gustavozreis/meufolego/MainActivity.kt
@@ -2,10 +2,25 @@ package com.gustavozreis.meufolego
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // define o controlador de navegação
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_graph) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // função que altera o título da action bar automaticamente
+        setupActionBarWithNavController(navController)
+
     }
 }
---
+++ app/src/main/java/com/gustavozreis/meufolego/fragments/RecordsFragment.kt
@@ -0,0 +1,6 @@
+package com.gustavozreis.meufolego.fragments
+
+import androidx.fragment.app.Fragment
+
+class RecordsFragment: Fragment() {
+}
