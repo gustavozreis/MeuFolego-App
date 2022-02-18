308250e590813e104a9eab34bff98ada4f848f23
gustavozreis <gzreis@gmail.com>
2022-02-18 20:10:14 -0300

    Configurado viewbinging dos fragments

--- app/src/main/java/com/gustavozreis/meufolego/fragments/RecordsFragment.kt
+++ app/src/main/java/com/gustavozreis/meufolego/fragments/RecordsFragment.kt
@@ -1,6 +1,34 @@
 package com.gustavozreis.meufolego.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.gustavozreis.meufolego.databinding.FragmentRecordsBinding
 
 class RecordsFragment: Fragment() {
+
+    var binding: FragmentRecordsBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRecordsBinding.inflate(inflater, container, false)
+        return binding?.root // vincula o fragment ao layout
+    }
+
+    // Retorna o valor do binding para nulo
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+
 }
--- app/src/main/java/com/gustavozreis/meufolego/fragments/StartFragment.kt
+++ app/src/main/java/com/gustavozreis/meufolego/fragments/StartFragment.kt
@@ -1,6 +1,33 @@
 package com.gustavozreis.meufolego.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.gustavozreis.meufolego.databinding.FragmentStartBinding
 
 class StartFragment: Fragment() {
+
+    private var binding: FragmentStartBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding?.root // vincula o fragment ao layout
+    }
+
+    // Retorna o valor do binding para nulo
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
 }
