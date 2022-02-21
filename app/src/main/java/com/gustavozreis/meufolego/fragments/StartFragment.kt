9bcbac5049cfc5a52340884cfc266bd6b02c9471
gustavozreis <gzreis@gmail.com>
2022-02-21 16:33:51 -0300

    Configurado botao de início do cronometro

--- app/src/main/java/com/gustavozreis/meufolego/fragments/StartFragment.kt
+++ app/src/main/java/com/gustavozreis/meufolego/fragments/StartFragment.kt
@@ -1,15 +1,33 @@
 package com.gustavozreis.meufolego.fragments
 
+import android.annotation.SuppressLint
+import android.app.usage.UsageEvents
+import android.content.Context
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.MotionEvent.*
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Chronometer
+import android.widget.ImageButton
+import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.gustavozreis.meufolego.databinding.FragmentStartBinding
+import com.gustavozreis.meufolego.viewmodel.TimeViewModel
 
 class StartFragment: Fragment() {
 
-    private var binding: FragmentStartBinding? = null
+    private var binding: FragmentStartBinding? = null // vinculação de visualização
+
+    private val timeViewModel: TimeViewModel by activityViewModels() // instância do viewModel
+
+    var cronometro: Chronometer? = null
+
+    var botao: ImageButton? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +42,38 @@ class StartFragment: Fragment() {
         return binding?.root // vincula o fragment ao layout
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cronometro = binding?.crTempoPassado
+
+        botao = binding?.ibImageButton
+
+        botao?.setOnTouchListener { _, motionEvent ->
+            if (motionEvent.action == ACTION_DOWN) iniciaCronometro()
+            if (motionEvent.action == ACTION_UP) paraCronometro()
+            true
+        }
+
+
+    }
+
+
+    /*
+    Função que inicia o cronometro
+     */
+    fun iniciaCronometro() {
+        cronometro?.start()
+    }
+
+    /*
+    Função que pausa o cronometro
+     */
+    fun paraCronometro() {
+        cronometro?.stop()
+    }
+
+
     // Retorna o valor do binding para nulo
     override fun onDestroyView() {
         super.onDestroyView()
@@ -31,3 +81,6 @@ class StartFragment: Fragment() {
     }
 
 }
+
+
+
---
+++ app/src/main/java/com/gustavozreis/meufolego/viewmodel/TimeViewModel.kt
@@ -0,0 +1,10 @@
+package com.gustavozreis.meufolego.viewmodel
+
+import android.os.CountDownTimer
+import android.widget.Chronometer
+import androidx.lifecycle.ViewModel
+
+class TimeViewModel : ViewModel() {
+
+
+}
