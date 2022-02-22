30058192488f388937c6da5b639f5dd7df3c818c
gustavozreis <gzreis@gmail.com>
2022-02-22 14:12:05 -0300

    Desenvolvimento da UI do fragment de recordes.

---
+++ app/src/main/java/com/gustavozreis/meufolego/adapters/RecordListAdapter.kt
@@ -0,0 +1,9 @@
+package com.gustavozreis.meufolego.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+
+class RecordListAdapter : RecyclerView.Adapter<RecordListAdapter.RecordListViewHolder> {
+
+
+
+}
